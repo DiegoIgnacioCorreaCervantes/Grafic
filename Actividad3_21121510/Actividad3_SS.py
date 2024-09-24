90d1f19b0b2efd0cc06881cdc36cc7aef63485c1
DiegoIgnacioCorreaCervantes <diegoignaciocorreacervantes@gmail.com>
2024-09-23 18:21:39 -0600

    Practica 3

---
+++ Actividad3_21121510/Actividad3_SS.py
@@ -0,0 +1,76 @@
+#Parametricas Sistema solar
+import numpy as np
+import cv2
+
+# Función para generar un solo punto de la elipse en función del parámetro t
+def generar_punto_elipse(a, b, t):
+    x = int(a * np.cos(t) + 540)  # Desplazamiento para centrar
+    y = int(b * np.sin(t) + 300)
+    return (x, y)
+
+# Dimensiones de la imagen
+img_width, img_height = 1100, 650
+
+# Crear una imagen en blanco
+imagen = np.zeros((img_height, img_width, 3), dtype=np.uint8)
+
+# Parámetros de la elipse
+a = 200  # Semieje mayor
+b = 100  # Semieje menor
+num_puntos = 1000
+
+# Crear los valores del parámetro t para la animación
+t_vals = np.linspace(0, 2 * np.pi, num_puntos)
+
+# Bucle de animación
+for t in t_vals:
+    
+    # Crear una nueva imagen en blanco en cada iteración
+    imagen = np.zeros((img_height, img_width, 3), dtype=np.uint8)
+    
+    #Sol
+    cv2.circle(imagen, (550, 295), radius=60, color=(0, 255, 255), thickness=-1)
+
+    # Generar el punto en la elipse
+    punto = generar_punto_elipse(a, b, t)
+
+    punto1 = generar_punto_elipse(a+50, b+50, t)
+
+    punto2 = generar_punto_elipse(a+100, b+100, t)
+
+    punto3 = generar_punto_elipse(a+150, b+150, t)
+    
+    # Dibujar el punto en la elipse
+    cv2.circle(imagen, punto, radius=20, color=(0, 255, 0), thickness=-1)
+
+    cv2.circle(imagen, punto1, radius=15, color=(0, 0, 255), thickness=-1)
+
+    cv2.circle(imagen, punto2, radius=10, color=(150, 255, 255), thickness=-1)
+
+    cv2.circle(imagen, punto3, radius=5, color=(255, 0, 0), thickness=-1)
+    
+    # Dibujar la trayectoria completa de la elipse (opcional, si quieres ver toda la elipse)
+    for t_tray in t_vals:
+        pt_tray = generar_punto_elipse(a, b, t_tray)
+        cv2.circle(imagen, pt_tray, radius=1, color=(255, 255, 255), thickness=-1)
+
+    for t_tray in t_vals:
+        pt_tray = generar_punto_elipse(a+50, b+50, t_tray)
+        cv2.circle(imagen, pt_tray, radius=1, color=(255, 255, 255), thickness=-1)
+
+    for t_tray in t_vals:
+        pt_tray = generar_punto_elipse(a+100, b+100, t_tray)
+        cv2.circle(imagen, pt_tray, radius=1, color=(255, 255, 255), thickness=-1)
+
+    for t_tray in t_vals:
+        pt_tray = generar_punto_elipse(a+150, b+150, t_tray)
+        cv2.circle(imagen, pt_tray, radius=1, color=(255, 255, 255), thickness=-1)
+    
+    # Mostrar la imagen con el punto en movimiento
+    cv2.imshow('img', imagen)
+    
+    # Controlar la velocidad de la animación (en milisegundos)
+    cv2.waitKey(5)
+
+# Cerrar la ventana después de la animación
+cv2.destroyAllWindows()
